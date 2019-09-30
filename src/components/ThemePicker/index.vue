4266b878b7ba5139d5b3ac885a16057e9416bff8
lbb1811 <liubin_2014@outlook.com>
2019-09-30 17:09:01 +0800

    自定义主题组件

---
+++ src/components/ThemePicker/index.vue
@@ -0,0 +1,158 @@
+<template>
+  <el-color-picker v-model="theme" class="theme-picker" popper-clas="theme-picker-dropdown"
+    :predefine="['#409EFF', '#1890ff', '#304156','#212121','#11a983', '#13c2c2', '#6959CD', '#f5222d', ]"
+  />
+</template>
+
+<script>
+// element-ui version from node_modules
+const version = require('element-ui/package.json').version
+const ORIGINAL_THEME = '#409EFF' // default color
+export default {
+  data () {
+    return {
+      chalk: '', // content of theme-chalk css
+      theme: ''
+    }
+  },
+  computed: {
+    defaultTheme () {
+      return this.$store.state.settings.theme
+    }
+  },
+  watch: {
+    defaultTheme: {
+      handler: function (val, oldVal) {
+        this.theme = val
+      },
+      immediate: true
+    },
+    async theme (val) {
+      const oldVal = this.chalk ? this.theme : ORIGINAL_THEME
+      if (typeof val !== 'string') return
+      // 获取选择的主题色及浅色深色
+      const themeCluster = this.getThemeCluster(val.replace('#', ''))
+      // 获取旧的主题色及浅色深色
+      const originalCluster = this.getThemeCluster(oldVal.replace('#', ''))
+      console.log('themeCluster, originalCluster ', themeCluster, originalCluster)
+
+      const $message = this.$message({
+        message: '  Compiling the theme',
+        customClass: 'theme-message',
+        type: 'success',
+        duration: 0,
+        iconClass: 'el-icon-loading'
+      })
+      /**
+       * 为 id 的元素更换样式 (换肤)
+       */
+      const getHandler = (variable, id) => {
+        return () => {
+          const originalCluster = this.getThemeCluster(ORIGINAL_THEME.replace('#', ''))
+          const newStyle = this.updateStyle(this[variable], originalCluster, themeCluster)
+          let styleTag = document.getElementById(id) // style 标签
+          if (!styleTag) {
+            styleTag = document.createElement('style')
+            styleTag.setAttribute('id', id)
+            document.head.appendChild(styleTag)
+          }
+          styleTag.innerText = newStyle
+        }
+      }
+      if (!this.chalk) { // 保存样式，如果没有，ajax取
+        const url = `https://unpkg.com/element-ui@${version}/lib/theme-chalk/index.css`
+        await this.getCSSString(url, 'chalk')
+      }
+      const chalkHandler = getHandler('chalk', 'chalk-style')
+      chalkHandler()
+
+      const styles = [].slice.call(document.querySelectorAll('style'))
+        .filter(style => {
+          const text = style.innerText
+          return new RegExp(oldVal, 'i').test(text) && !/Chalk Variables/.test(text)
+        })
+      styles.forEach(style => {
+        const { innerText } = style
+        if (typeof innerText !== 'string') return
+        style.innerText = this.updateStyle(innerText, originalCluster, themeCluster)
+      })
+
+      this.$emit('change', val)
+      $message.close()
+    }
+  },
+  methods: {
+    /**
+     * 更新样式  将样式中旧的颜色替换为新的颜色； 返回新样式
+     */
+    updateStyle (style, oldCluster, newCluster) {
+      let newStyle = style
+      oldCluster.forEach((color, index) => {
+        newStyle = newStyle.replace(new RegExp(color, 'ig'), newCluster[index])
+      })
+      return newStyle
+    },
+    // 从 url 获取的CSS字符串
+    getCSSString (url, variable) {
+      return new Promise(resolve => {
+        // 为何不改用 axios
+        const xhr = new XMLHttpRequest()
+        xhr.onreadystatechange = () => {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            // 正则匹配 [^xyz] 非xyz    (去掉font-face部分)
+            this[variable] = xhr.responseText.replace(/@font-face{[^}]+}/, '')
+            resolve()
+          }
+        }
+        xhr.open('GET', url)
+        xhr.send()
+      })
+    },
+    /**
+     * 将主题色 转换为浅色、深色，一并返回。
+     */
+    getThemeCluster (theme) {
+      // 浅色调
+      const tintColor = (color, tint) => {
+        // 转为 16 进制
+        let red = parseInt(color.slice(0, 2), 16)
+        let green = parseInt(color.slice(2, 4), 16)
+        let blue = parseInt(color.slice(4, 6), 16)
+        if (tint === 0) {
+          // when primary color is in its rgb space
+          return [red, green, blue].join(',')
+        } else {
+          red += Math.round(tint * (255 - red))
+          green += Math.round(tint * (255 - green))
+          blue += Math.round(tint * (255 - blue))
+          red = red.toString(16)
+          green = green.toString(16)
+          blue = blue.toString(16)
+          return `#${red}${green}${blue}`
+        }
+      }
+      // 暗(深)色调
+      const shadeColor = (color, shade) => {
+        let red = parseInt(color.slice(0, 2), 16)
+        let green = parseInt(color.slice(2, 4), 16)
+        let blue = parseInt(color.slice(4, 6), 16)
+        red = Math.round((1 - shade) * red)
+        green = Math.round((1 - shade) * green)
+        blue = Math.round((1 - shade) * blue)
+        red = red.toString(16)
+        green = green.toString(16)
+        blue = blue.toString(16)
+
+        return `#${red}${green}${blue}`
+      }
+
+      const clusters = [theme]
+      for (let i = 0; i <= 9; i++) {
+        clusters.push(tintColor(theme, Number((i / 10).toFixed(2))))
+      }
+      clusters.push(shadeColor(theme, 0.1))
+      return clusters
+    }
+  }
+}
+</script>
